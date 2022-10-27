d297561e78e56e71d9f97eeedc1cb2d310777f31
ignaciojarruza <ignaciojarruza@gmail.com>
2022-10-26 20:32:33 -0400

    Updated UserDao because of mongoose version

--- daos/UserDao.ts
+++ daos/UserDao.ts
@@ -14,35 +14,54 @@ export default class UserDao implements UserDaoI {
      * Handles calls from the controller. Retrieves a list of all users.
      * @return the list of users
      */
-   async findAllUsers(): Promise<User[]> {
-       return await UserModel.find();
-   }
+     async findAllUsers(): Promise<User[]> {
+        const userMongooseModels = await UserModel.find();
+        const userModels = userMongooseModels
+            .map((userMongooseModel) => {
+                return new User(
+                    userMongooseModel?._id.toString()??'',
+                    userMongooseModel?.username??'',
+                    userMongooseModel?.password??'',
+                );
+            });
+        return userModels;
+    }
 
    /**
     * Handles data from the controller. Retrieves a user.
     * @return the user with the id provided
     */
-   async findUserById(uid: string): Promise<any> {
-       return await UserModel.findById(uid);
-   }
+    async findUserById(uid: string): Promise<User> {
+        const userMongooseModel = await UserModel.findById(uid);
+        return new User(
+            userMongooseModel?._id.toString()??'',
+            userMongooseModel?.username??'',
+            userMongooseModel?.password??'',
+        );
+    }
 
    /**
     * Handles the data from the controller and creates a record in the database.
     * @param user the user details (from the body)
     * @return the user record created
     */
-   async createUser(user: User): Promise<User> {
-       return await UserModel.create(user);
-   }
+    async createUser(user: User): Promise<User> {
+        const userMongooseModel = await UserModel.create(user);
+        return new User(
+            userMongooseModel?._id.toString()??'',
+            userMongooseModel?.username??'',
+            userMongooseModel?.password??'',
+        );
+    }
 
    /**
     * Handles the data from the controller and removes a record from the database.
     * @param uid the id of the user
     * @return the deletion status
     */
-   async deleteUser(uid: string):  Promise<any> {
-       return await UserModel.deleteOne({_id: uid});
-   }
+    async deleteUser(uid: string):  Promise<any> {
+        return await UserModel.deleteOne({_id: uid});
+    }
 
    /**
     * Handles the data from the controller and updates a record from the database.
@@ -50,7 +69,10 @@ export default class UserDao implements UserDaoI {
     * @param user the user details to update with (from the BODY)\
     * @return the update status
     */
-   async updateUser(uid: string, user: User): Promise<any> {
-       return await UserModel.updateOne({_id: uid}, {$set: user});
-   }
+    async updateUser(uid: string, user: User): Promise<any> {
+        return await UserModel.updateOne({_id: uid}, {$set: {
+                username: user.uName,
+                password: user.pass
+            }});
+    }
 }
